82e556701266c086b2fbe8fdfe07863cea2a1cdc
Brian Lopez <seniorlopez@gmail.com>
2012-11-19 17:33:15 -0800

    port over to the official snappy lib

--- ext/ohsnap/ext.c
+++ ext/ohsnap/ext.c
@@ -1,10 +1,8 @@
 #include <ruby.h>
-#include "snappy.h"
+#include "snappy-c.h"
 
 static VALUE rb_mOhsnap_compress(VALUE self, VALUE rb_data) {
-	struct snappy_env env;
-	size_t max_compressed_size=0;
-	int err;
+	snappy_status err;
 	const char *data;
 	char *out;
 	size_t out_len=0;
@@ -15,21 +13,15 @@ static VALUE rb_mOhsnap_compress(VALUE self, VALUE rb_data) {
 	data     = RSTRING_PTR(rb_data);
 	data_len = RSTRING_LEN(rb_data);
 
-	err = snappy_init_env(&env);
-	if (err < 0) {
-		rb_raise(rb_eNoMemError, "Out of memory");
-	}
+	out_len = snappy_max_compressed_length(data_len);
+	out = (char*)xmalloc(out_len);
 
-	max_compressed_size = snappy_max_compressed_length(data_len);
-	out = xmalloc(max_compressed_size);
+	err = snappy_compress(data, data_len, out, &out_len);
 
-	err = snappy_compress(&env, data, data_len, out, &out_len);
-	if (err < 0) {
+	if (err != SNAPPY_OK) {
 		xfree(out);
 		rb_raise(rb_eIOError, "There was an error compressing the data");
 	}
-
-	snappy_free_env(&env);
 
 	rb_out = rb_str_new(out, out_len);
 	xfree(out);
@@ -38,8 +30,7 @@ static VALUE rb_mOhsnap_compress(VALUE self, VALUE rb_data) {
 }
 
 static VALUE rb_mOhsnap_decompress(VALUE self, VALUE rb_compressed_data) {
-	bool ret;
-	int err;
+	snappy_status err;
 	const char *compressed_data;
 	size_t compressed_data_len=0;
 	char *out;
@@ -50,14 +41,14 @@ static VALUE rb_mOhsnap_decompress(VALUE self, VALUE rb_compressed_data) {
 	compressed_data     = RSTRING_PTR(rb_compressed_data);
 	compressed_data_len = RSTRING_LEN(rb_compressed_data);
 
-	ret = snappy_uncompressed_length(compressed_data, compressed_data_len, &out_len);
-	if (!ret) {
+	err = snappy_uncompressed_length(compressed_data, compressed_data_len, &out_len);
+	if (err != SNAPPY_OK) {
 		rb_raise(rb_eArgError, "Unable to determine uncompressed length, input is corrupt");
 	}
 
-	out = xmalloc(out_len);
-	err = snappy_uncompress(compressed_data, compressed_data_len, out);
-	if (err < 0) {
+	out = (char*)xmalloc(out_len);
+	err = snappy_uncompress(compressed_data, compressed_data_len, out, &out_len);
+	if (err != SNAPPY_OK) {
 		xfree(out);
 		rb_raise(rb_eIOError, "There was an error uncompressing the data");
 	}
@@ -71,6 +62,6 @@ static VALUE rb_mOhsnap_decompress(VALUE self, VALUE rb_compressed_data) {
 void Init_ohsnap() {
 	VALUE rb_mOhsnap = rb_define_module("Ohsnap");
 
-	rb_define_method(rb_mOhsnap, "compress", rb_mOhsnap_compress, 1);
-	rb_define_method(rb_mOhsnap, "decompress", rb_mOhsnap_decompress, 1);
+	rb_define_method(rb_mOhsnap, "compress", (VALUE(*)(ANYARGS))rb_mOhsnap_compress, 1);
+	rb_define_method(rb_mOhsnap, "decompress", (VALUE(*)(ANYARGS))rb_mOhsnap_decompress, 1);
 }
--- ext/ohsnap/extconf.rb
+++ ext/ohsnap/extconf.rb
@@ -1,9 +1,52 @@
 require 'mkmf'
-require 'rbconfig'
 
-have_header "byteswap.h"
-have_header "CoreFoundation/CoreFoundation.h"
+def sys(cmd)
+  puts "  -- #{cmd}"
+  unless ret = xsystem(cmd)
+    raise "#{cmd} failed, please report issue on http://github.com/brianmario/ohsnap"
+  end
+  ret
+end
 
-$CFLAGS << ' -Wall -Wextra -O0 -ggdb3' if ENV['DEBUG']
+if `which make`.strip.empty?
+  STDERR.puts "\n\n"
+  STDERR.puts "***************************************************************************************"
+  STDERR.puts "*************** make required (apt-get install make build-essential) =( ***************"
+  STDERR.puts "***************************************************************************************"
+  exit(1)
+end
+
+snappy_ver = "snappy-1.0.5"
+src = File.basename("#{snappy_ver}.tar.gz")
+dir = File.basename(src, '.tar.gz')
+
+CWD = File.expand_path(File.dirname(__FILE__))
+Dir.chdir("#{CWD}/src") do
+  FileUtils.rm_rf(dir) if File.exists?(dir)
+
+  sys("tar zxvf #{src}")
+  Dir.chdir(dir) do
+    sys("./configure --prefix=#{CWD}/dst/ --disable-shared --enable-static --with-pic")
+    sys("make install")
+  end
+end
+
+FileUtils.cp "#{CWD}/dst/lib/libsnappy.a", "#{CWD}/libsnappy_ext.a"
+
+$INCFLAGS[0,0] = " -I#{CWD}/dst/include "
+$LDFLAGS << " -L#{CWD} "
+
+dir_config 'snappy'
+unless have_library 'snappy_ext' and have_header 'snappy-c.h'
+  STDERR.puts "\n\n"
+  STDERR.puts "***************************************************************************************"
+  STDERR.puts "********* error compiling and linking libsnappy. please report issue on github ********"
+  STDERR.puts "***************************************************************************************"
+  exit(1)
+end
+
+CONFIG['LDSHARED'] = CONFIG['LDSHAREDXX']
+$CFLAGS << ' -Wall'
+$CFLAGS << ' -Wextra -O0 -ggdb3' if ENV['DEBUG']
 
 create_makefile("ohsnap/ohsnap")
